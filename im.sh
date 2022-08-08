1287f2f83594dc05eb44c658d1d96f2ec8d68d8a
aaa1230217 <58129947+aaa1230217@users.noreply.github.com>
2022-08-09 03:47:29 +0800

    Update im.sh

--- im.sh
+++ im.sh
@@ -7,10 +7,6 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=============================================================
-
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.252/g' package/base-files/files/bin/config_generate
 git clone https://github.com/QiuSimons/openwrt-mos.git package/openwrt-mos
-
-
-
