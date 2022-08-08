d7b6d601d4810f454ad8c9faa53201ad479e3547
aaa1230217 <58129947+aaa1230217@users.noreply.github.com>
2022-08-09 03:43:50 +0800

    Add files via upload

---
+++ im.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#=============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=============================================================
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.50.252/g' package/base-files/files/bin/config_generate
+git clone https://github.com/QiuSimons/openwrt-mos.git package/openwrt-mos
+
+
+
