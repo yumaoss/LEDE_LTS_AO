51f961780842d3d64b317eb11dfc2c74621f4c29
aaa1230217 <58129947+aaa1230217@users.noreply.github.com>
2022-08-10 21:34:25 +0800

    Update immosdns1.sh

--- immosdns1.sh
+++ immosdns1.sh
@@ -9,14 +9,14 @@
 #=============================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.252/g' package/base-files/files/bin/config_generate
-sed -i 's/tools-y += patchelf/tools-y += patchelf upx ucl/g' tools/Makefile
-mkdir -p tools/upx
-mkdir -p tools/ucl
-mkdir -p tools/ucl/patches
-wget https://raw.githubusercontent.com/aaa1230217/tools/main/upx/Makefile -nv -O tools/upx/Makefile
-wget https://raw.githubusercontent.com/aaa1230217/tools/main/ucl/Makefile -nv -O tools/ucl/Makefile
-wget https://raw.githubusercontent.com/aaa1230217/tools/main/ucl/patches/100-fix-apple-m1-support.patch -nv -O tools/ucl/patches/100-fix-apple-m1-support.patch
-wget https://raw.githubusercontent.com/aaa1230217/tools/main/ucl/patches/200-ucl-apple-m1-configsub-fix.patch -nv -O tools/ucl/patches/200-ucl-apple-m1-configsub-fix.patch
+#sed -i 's/tools-y += patchelf/tools-y += patchelf upx ucl/g' tools/Makefile
+#mkdir -p tools/upx
+#mkdir -p tools/ucl
+#mkdir -p tools/ucl/patches
+#wget https://raw.githubusercontent.com/aaa1230217/tools/main/upx/Makefile -nv -O tools/upx/Makefile
+#wget https://raw.githubusercontent.com/aaa1230217/tools/main/ucl/Makefile -nv -O tools/ucl/Makefile
+#wget https://raw.githubusercontent.com/aaa1230217/tools/main/ucl/patches/100-fix-apple-m1-support.patch -nv -O tools/ucl/patches/100-fix-apple-m1-support.patch
+#wget https://raw.githubusercontent.com/aaa1230217/tools/main/ucl/patches/200-ucl-apple-m1-configsub-fix.patch -nv -O tools/ucl/patches/200-ucl-apple-m1-configsub-fix.patch
 #git clone https://github.com/QiuSimons/openwrt-mos.git package/openwrt-mos
 git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/luci-theme-edge
 git clone https://github.com/QiuSimons/openwrt-mos.git package/openwrt-mos
