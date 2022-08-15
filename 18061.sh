aead80a5604534f3dbfb589c3e26b1d3f4ef3942
aaa1230217 <58129947+aaa1230217@users.noreply.github.com>
2022-08-16 04:14:57 +0800

    Add files via upload

---
+++ 18061.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#=============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=============================================================
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.50.252/g' package/base-files/files/bin/config_generate
+#sed -i 's/tools-y += patchelf/tools-y += patchelf upx ucl/g' tools/Makefile
+#mkdir -p tools/upx
+#mkdir -p tools/ucl
+#mkdir -p tools/ucl/patches
+#wget https://raw.githubusercontent.com/aaa1230217/tools/main/upx/Makefile -nv -O tools/upx/Makefile
+#wget https://raw.githubusercontent.com/aaa1230217/tools/main/ucl/Makefile -nv -O tools/ucl/Makefile
+#wget https://raw.githubusercontent.com/aaa1230217/tools/main/ucl/patches/100-fix-apple-m1-support.patch -nv -O tools/ucl/patches/100-fix-apple-m1-support.patch
+#wget https://raw.githubusercontent.com/aaa1230217/tools/main/ucl/patches/200-ucl-apple-m1-configsub-fix.patch -nv -O tools/ucl/patches/200-ucl-apple-m1-configsub-fix.patch
+#git clone https://github.com/QiuSimons/openwrt-mos.git package/openwrt-mos
+#git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/luci-theme-edge
+git clone https://github.com/QiuSimons/openwrt-mos.git package/new/openwrt-mos
+rm -rf ./package/new/openwrt-mos/mosdns
+sed -i 's/+mosdns-neo/+mosdns/g' package/new/openwrt-mos/luci-app-mosdns/Makefile
+curl -fsSL  https://raw.githubusercontent.com/aaa1230217/KPR_Fork_mosdns-diy/main/cus_config.yaml > ./package/new/openwrt-mos/luci-app-mosdns/root/etc/mosdns/cus_config.yaml
---
+++ 18062.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#============================================================
+rm -rf ./feeds/packages/net/v2ray-geodata
+rm -rf ./feeds/packages/net/mosdns/files
+sed -i '42, 47d' feeds/packages/net/mosdns/Makefile
